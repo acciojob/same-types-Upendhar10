95797f868aae582b79658072e9492caa2e71ec82
N.Upendhar <80301044+Upendhar10@users.noreply.github.com>
2025-06-13 15:56:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 function isSameType(value1, value2) {
 
+	// convert Strings into actual types
+	const actualVal1 = parseInput(value1);
+	const actualVal2 = parseInput(value2);
+	
+
 	// check for NaN explicitly
-	if(Number.isNaN(value1) && Number.isNaN(value2)){
-		return true;
+	if(Number.isNaN(actualVal1) || Number.isNaN(actualVal2)){
+		return Number.isNaN(actualVal1) && Number.isNaN(actualVal2);
 	}
 
 	// Check if both types are the same
 	return typeof(value1) === typeof(value2);
+}
+
+function parseInput(value){
+
+	// special handling for NaN
+	if(value === "NaN") return NaN;
+
+	try {
+		return JSON.parse(value);
+	} catch (error) {
+		// if it can't be parsed, return as a string
+		return value;
+	}
 }
 
 // do not change the code below.
